32818cf515c47bab0faeae78c6bed88dff641b0b
hugoConceicaoPT <hugoconceicao.TI@outlook.com>
2024-04-10 18:34:06 +0100

    Teste e função increaseSize() feito

--- src/SnakeLayer/Snake.java
+++ src/SnakeLayer/Snake.java
@@ -18,17 +18,18 @@ public class Snake {
         Ponto ponto2 = ultimoQuadrado.pontos.get(1);
         int arestaLength = ponto1.dist(ponto2);
         Quadrado novoQuadrado = new Quadrado(ultimoQuadrado.getPontos());
-        if (direction == Direction.UP) {
-            novoQuadrado.translate(0, -arestaLength);
-        } 
-        else if(direction == Direction.LEFT) {
-            novoQuadrado.translate(arestaLength, 0);
-        }
-        else if(direction == Direction.RIGHT) {
-            novoQuadrado.translate(-arestaLength, 0);
-        }
-        else {
-            novoQuadrado.translate(0, arestaLength);
+        switch (direction) {
+            case UP:
+                novoQuadrado.translate(0, -arestaLength); 
+                break;
+            case DOWN:
+                novoQuadrado.translate(0, arestaLength);
+            case LEFT:
+                novoQuadrado.translate(arestaLength, 0);
+                break;
+            case RIGHT:
+                novoQuadrado.translate(-arestaLength, 0);
+                break;
         }
         this.listaQuadrados.add(novoQuadrado);
     }
--- src/Tests/SnakeTest.java
+++ src/Tests/SnakeTest.java
@@ -13,8 +13,8 @@ public class SnakeTest {
     @Test
     public void increaseSizeTest() {
         String input1 = "1 1 1 3 3 3 3 1";
-        String input2 = "3 3 3 1 5 3 5 1";
-        String input3 = "5 3 5 1 7 3 7 1";
+        String input2 = "3 3 3 1 5 1 5 3";
+        String input3 = "5 3 5 1 7 1 7 3";
         List<Quadrado> quadrado = new ArrayList<>();
         quadrado.add(new Quadrado(input1));
         quadrado.add(new Quadrado(input2));
