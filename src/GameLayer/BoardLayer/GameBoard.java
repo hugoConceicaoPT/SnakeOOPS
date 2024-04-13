c600e0165b6a6acdad4caafc7e3e2b8c79581f87
hugoConceicaoPT <hugoconceicao.TI@outlook.com>
2024-04-13 21:42:06 +0100

    Comentários acerca das funções feitas

--- src/GameLayer/BoardLayer/GameBoard.java
+++ src/GameLayer/BoardLayer/GameBoard.java
@@ -1,11 +1,11 @@
 package GameLayer.BoardLayer;
 
 import java.util.List;
+import java.util.Random;
 
 import GameLayer.SnakeLayer.Retangulo;
 import GameLayer.SnakeLayer.Score;
 import GameLayer.SnakeLayer.Snake;
-import GameLayer.SnakeLayer.SnakeGame;
 
 public class GameBoard {
     private List<Obstacle> listOfObstacles; 
@@ -14,13 +14,22 @@ public class GameBoard {
     private int columns;
     private String foodType;
     private Cell [][] board;
-    private SnakeGame snake;
+    private Snake snake;
+    private Score score;
 
-
-    public GameBoard (List<Obstacle> listofObstacles,String foodType,int rows, int columns) {
+    /** Construtor para criar uma board no jogo
+     * @param listofObstacles lista de obstáculos
+     * @param foodType tipo de comida
+     * @param snake cobra
+     * @param rows linhas
+     * @param columns colunas
+     */
+    public GameBoard (List<Obstacle> listofObstacles,String foodType,Snake snake, int rows, int columns) {
         this.listOfObstacles = listofObstacles;
         this.foodType = foodType;
         this.rows = rows;
+        this.score = new Score(0);
+        this.snake = snake;
         this.columns = columns;
         this.board = new Cell[rows][columns];
         for (int i = 0; i < rows; i++) {
@@ -30,21 +39,31 @@ public class GameBoard {
         }
     }
 
+    /** Verifica se a cobra colida com as paredes da board ou com o obstáculo
+     * @return
+     */
     public boolean snakeCollided() {return true;}
+
+    /** Verifica se a comida esta contida na cobra
+     * @return verdadeiro se estiver contido, falso se não
+     * @throws CloneNotSupportedException
+     */
     public boolean foodContainedInSnake() throws CloneNotSupportedException {
-        if (food.FoodIntersetaHead(snake.getSnake())) {
-            snake.getSnake().increaseSize();
-            snake.getScore().increaseScore();
+        if (food.FoodIntersetaHead(snake)) {
+            snake.increaseSize();
+            score.increaseScore();
            return true; 
         }
         return false; 
     }
- /*   public void generateFood() {
 
+    /** Gera uma comida aleatória na board */
+    public void generateFood() {
+        Random random = new Random();
         boolean isEmpty = true;
         while (isEmpty) {
-            int row = (int)(Math.random * rows);
-            int column = (int)(Math.random *columns);
+            int row = random.nextInt(rows);
+            int column = random.nextInt(columns);
             Cell cellFood = new Cell(row, column);
             if (cellFood.getCellType() == CellType.EMPTY) {
                 cellFood.setCellType(CellType.FOOD);
@@ -52,6 +71,7 @@ public class GameBoard {
             }
         }
     }
-    */
+
+    /** Gera um obstáculo aleatório na board */
     public void generateObstacle() {}
 }
--- src/GameLayer/BoardLayer/Obstacle.java
+++ src/GameLayer/BoardLayer/Obstacle.java
@@ -6,6 +6,10 @@ public class Obstacle {
     private IFiguraGeometrica figuraGeometrica;
     private boolean isDynamic;
 
+    /** Construtor para criar um obstáculo
+     * @param figuraGeometrica figura geométrica
+     * @param isDynamic booleano para verificar se o obstáculo é dinâmico
+     */
     public Obstacle(IFiguraGeometrica figuraGeometrica, boolean isDynamic) {
         this.figuraGeometrica = figuraGeometrica;
         this.isDynamic = isDynamic;
