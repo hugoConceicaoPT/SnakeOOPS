28080d4450ea30eb651d0e340e3bb2153b6293ed
hugoConceicaoPT <hugoconceicao.TI@outlook.com>
2024-04-15 14:35:05 +0100

    Atualização

--- src/GameLayer/SnakeLayer/Snake.java
+++ src/GameLayer/SnakeLayer/Snake.java
@@ -54,7 +54,7 @@ public class Snake {
      */
     public boolean collidedWithHerself() {
         for (int i = 0; i < tail.size(); i++) {
-            if(head.interseta(tail.get(i)))
+            if(head.contido(tail.get(i)))
                 return true;
         }
         return false;
