ad265c26ac1fa0c94f21cd6e9ac3d484ed6e1e15
hugoConceicaoPT <hugoconceicao.TI@outlook.com>
2024-04-21 12:55:33 +0100

    Update CircunferenciaTest.java

--- src/Tests/CircurferenciaTest.java
+++ src/Tests/CircurferenciaTest.java
@@ -26,32 +26,32 @@ public class CircurferenciaTest {
     }
     @Test
     public void contidaNaCircunferenciaTest(){
-    Circunferencia maiorCirculo = new Circunferencia(new Ponto(5, 5), 5);
-    Circunferencia menorCirculo = new Circunferencia(new Ponto(7, 7), 2);
-    assertTrue(menorCirculo.contidaNaCircunferencia(maiorCirculo));
-    Circunferencia foraCirculo = new Circunferencia(new Ponto(10, 10), 1);
-    assertFalse(foraCirculo.contidaNaCircunferencia(maiorCirculo));
-    Circunferencia intersectaBorda = new Circunferencia(new Ponto(2, 5), 4);
-    assertFalse(intersectaBorda.contidaNaCircunferencia(maiorCirculo));
-    Circunferencia igualCirculo = new Circunferencia(new Ponto(5, 5), 5);
-    assertTrue(igualCirculo.contidaNaCircunferencia(maiorCirculo));
+        Circunferencia maiorCirculo = new Circunferencia(new Ponto(5, 5), 5);
+        Circunferencia menorCirculo = new Circunferencia(new Ponto(7, 7), 2);
+        assertTrue(menorCirculo.contidaNaCircunferencia(maiorCirculo));
+        Circunferencia foraCirculo = new Circunferencia(new Ponto(10, 10), 1);
+        assertFalse(foraCirculo.contidaNaCircunferencia(maiorCirculo));
+        Circunferencia intersectaBorda = new Circunferencia(new Ponto(2, 5), 4);
+        assertFalse(intersectaBorda.contidaNaCircunferencia(maiorCirculo));
+        Circunferencia igualCirculo = new Circunferencia(new Ponto(5, 5), 5);
+        assertTrue(igualCirculo.contidaNaCircunferencia(maiorCirculo));
     }
 
     @Test
-public void contidaNoPoligonoTest(){
-    List<Ponto> verticesPoligono = new ArrayList<>();
-    verticesPoligono.add(new Ponto(0, 0));
-    verticesPoligono.add(new Ponto(10, 0));
-    verticesPoligono.add(new Ponto(10, 10));
-    verticesPoligono.add(new Ponto(0, 10));
-    Poligono poligono = new Poligono(verticesPoligono);
-    Circunferencia circuloInterno = new Circunferencia(new Ponto(5, 5), 2);
-    assertTrue(circuloInterno.contidaNoPoligono(poligono));
-    Circunferencia circuloParcial = new Circunferencia(new Ponto(8, 8), 4);
-    assertFalse(circuloParcial.contidaNoPoligono(poligono));
-    Circunferencia circuloExterno = new Circunferencia(new Ponto(15, 15), 1);
-    assertFalse(circuloExterno.contidaNoPoligono(poligono));
-}
+    public void contidaNoPoligonoTest(){
+        List<Ponto> verticesPoligono = new ArrayList<>();
+        verticesPoligono.add(new Ponto(0, 0));
+        verticesPoligono.add(new Ponto(10, 0));
+        verticesPoligono.add(new Ponto(10, 10));
+        verticesPoligono.add(new Ponto(0, 10));
+        Poligono poligono = new Poligono(verticesPoligono);
+        Circunferencia circuloInterno = new Circunferencia(new Ponto(5, 5), 2);
+        assertTrue(circuloInterno.contidaNoPoligono(poligono));
+        Circunferencia circuloParcial = new Circunferencia(new Ponto(8, 8), 4);
+        assertFalse(circuloParcial.contidaNoPoligono(poligono));
+        Circunferencia circuloExterno = new Circunferencia(new Ponto(15, 15), 1);
+        assertFalse(circuloExterno.contidaNoPoligono(poligono));
+    }
 
 
 }
