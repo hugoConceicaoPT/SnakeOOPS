fc0cca0a118cb846bfc4effbde85fa06096ee8f9
joaoventura04 <joses4.js1@gmail.com>
2024-04-24 11:40:47 +0100

    Teste GenerationFood

--- src/Tests/GameBoardTest.java
+++ src/Tests/GameBoardTest.java
@@ -51,4 +51,16 @@ public class GameBoardTest {
         assertTrue(gameBoard.getListOfObstacles().size() > 0);
         assertFalse(snake.getHead().contida(gameBoard.getListOfObstacles().get(0).getPoligono()));
     }
+    @Test
+    public void generateFoodTest() throws CloneNotSupportedException{
+        String input = "8 5 8 3 6 3 6 5";
+        LinkedList<Quadrado> listaQuadrados = new LinkedList<>();
+        listaQuadrados.add(new Quadrado(input));
+        Snake snake = new Snake(listaQuadrados, true);
+        snake.setDirection(Direction.RIGHT);
+        snake.increaseSize();
+        GameBoard gameBoard = new GameBoard(snake, 200, 100,true,1,1,false);  
+        gameBoard.generateFood();
+        assertNotNull(gameBoard.getFood());
+    }
 }
