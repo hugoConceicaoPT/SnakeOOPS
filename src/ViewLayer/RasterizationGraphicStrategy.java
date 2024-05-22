38d8c8226f2e0da28eafa35d66175c19439f9e43
joaoventura04 <joses4.js1@gmail.com>
2024-05-22 15:31:58 +0100

    more comments

--- src/ViewLayer/RasterizationGraphicStrategy.java
+++ src/ViewLayer/RasterizationGraphicStrategy.java
@@ -4,6 +4,7 @@ import ModelLayer.BoardLayer.GameBoard;
 
 import javax.swing.*;
 import java.awt.*;
+
 /**
  * Classe abstrata que define a estratégia de rasterização gráfica para o jogo.
  * Responsabilidade: Definir os métodos abstratos para atualização dos obstáculos, cobra e comida no tabuleiro,
@@ -14,7 +15,7 @@ public abstract class RasterizationGraphicStrategy extends JPanel {
 
     protected GameBoard gameBoard;
     protected JPanel panel;
-    private final Image backgroundImage = new ImageIcon("assets/backgroundGame.png").getImage();
+    private final Image backgroundImage = new ImageIcon("assets/backgroundGame.png").getImage(); // Imagem de fundo do tabuleiro
 
     /**
      * Construtor que inicializa a estratégia de rasterização gráfica com o tabuleiro de jogo especificado.
@@ -23,9 +24,8 @@ public abstract class RasterizationGraphicStrategy extends JPanel {
     public RasterizationGraphicStrategy(GameBoard gameBoard) {
         this.gameBoard = gameBoard;
         this.panel = this;
-        this.panel.setPreferredSize(new Dimension(this.gameBoard.getWidthBoard(),this.gameBoard.getHeightBoard()));
+        this.panel.setPreferredSize(new Dimension(this.gameBoard.getWidthBoard(), this.gameBoard.getHeightBoard()));
     }
-
 
     /**
      * Método para renderizar os componentes gráficos do painel.
@@ -46,33 +46,55 @@ public abstract class RasterizationGraphicStrategy extends JPanel {
      * Cada estratégia de rasterização deve implementar esta lógica conforme necessário.
      */
     public abstract void updateObstacles();
+
     /**
      * Atualiza as células que representam a cobra no tabuleiro.
      * Cada estratégia de rasterização deve implementar esta lógica conforme necessário.
      */
     public abstract void updateSnake();
+
     /**
      * Atualiza as células que representam a comida no tabuleiro.
      * Cada estratégia de rasterização deve implementar esta lógica conforme necessário.
      */
     public abstract void updateFood();
 
+    /**
+     * Obtém o tabuleiro de jogo atual.
+     * @return O tabuleiro de jogo.
+     */
     public GameBoard getGameBoard() {
         return gameBoard;
     }
 
+    /**
+     * Define um novo tabuleiro de jogo.
+     * @param gameBoard O novo tabuleiro de jogo a ser definido.
+     */
     public void setGameBoard(GameBoard gameBoard) {
         this.gameBoard = gameBoard;
     }
 
+    /**
+     * Obtém o painel de exibição.
+     * @return O painel de exibição.
+     */
     public JPanel getPanel() {
         return panel;
     }
 
+    /**
+     * Define um novo painel de exibição.
+     * @param panel O novo painel de exibição a ser definido.
+     */
     public void setPanel(JPanel panel) {
         this.panel = panel;
     }
 
+    /**
+     * Obtém a imagem de fundo do tabuleiro.
+     * @return A imagem de fundo do tabuleiro.
+     */
     public Image getBackgroundImage() {
         return backgroundImage;
     }
